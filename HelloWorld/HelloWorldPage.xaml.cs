e4ad57dbfd91aa88fec67f52e5279d18ceb1791e
Casey <caseykelpinski@gmail.com>
2018-09-17 17:16:41 -0500

    HelloWorld git upload

---
+++ HelloWorld/HelloWorldPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class HelloWorldPage : ContentPage
+    {
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            if (lblDisplay.Text == "Hello World!")
+            {
+                lblDisplay.Text = "Goodbye World!";
+            }else
+            {
+                lblDisplay.Text = "Hello World!";
+            }
+        }
+
+        public HelloWorldPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
